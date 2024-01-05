c66280cf481184e5ed114b9938b2b6a539f1e2f9
SOURADEEP HAZRA <V22089@students.iitmandi.ac.in>
2024-01-06 00:36:18 +0530

    updated

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <Link rel="icon" href="/favicon-32x32.png" sizes="any" ></Link>
+      {/* <Link rel="icon" href="/favicon-32x32.png" sizes="any" ></Link> */}
 
         <Providers>
           <Header/>
