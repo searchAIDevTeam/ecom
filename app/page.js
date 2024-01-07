a1be4515f1ab6851f622743cc216bd4095bfc786
tahirshaikh85 <tahirshaikhrs7@gmail.com>
2024-01-07 14:54:37 +0530

    deviceId localstorage

--- app/page.js
+++ app/page.js
@@ -1,5 +1,9 @@
 import HomePage from "@/components/home/HomePage";
-
+import SaveDeviceIdLocalstorage from "@/utils/SaveDeviceIdLocalstorage ";
 export default function Home() {
-  return <HomePage />;
+
+  return <>
+    <SaveDeviceIdLocalstorage />
+    <HomePage />
+  </>;
 }
