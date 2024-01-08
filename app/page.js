f85e8581ba78ad5c7490f543738abe652deecf68
Karthik ks <karthikks260703@gmail.com>
2024-01-08 21:10:02 +0530

    Revert "added Splashscreen"
    This reverts commit 8103d6502b8e8a3c1bf2638e059fcd7dc1f8b7b9.

--- app/page.js
+++ app/page.js
@@ -1,33 +1,10 @@
-"use client";
-import Splashscreen from "@/components/Splashscreen/Splashscreen";
 import HomePage from "@/components/home/HomePage";
 import SaveDeviceIdLocalstorage from "@/utils/SaveDeviceIdLocalstorage ";
-import { useEffect, useState } from "react";
-import { createPortal } from "react-dom";
 export default function Home() {
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-  }, []);
-
   return (
     <>
-      {loading ? (
-        createPortal(
-          <main className="absolute w-screen h-screen z-[9999] bg-white top-0 left-0 right-0 bottom-0">
-            <Splashscreen />
-          </main>,
-          document.body
-        )
-      ) : (
-        <>
-          <SaveDeviceIdLocalstorage />
-          <HomePage />
-        </>
-      )}
+      <SaveDeviceIdLocalstorage />
+      <HomePage />
     </>
   );
 }
