53e39b8ebc2f6052c96a1159e449b21894a8364a
amaan35 <amaanlm35@gmail.com>
2024-01-08 13:56:44 +0530

    Menu integration, and removing shadow

--- components/NewHeader/NewHeader.js
+++ components/NewHeader/NewHeader.js
@@ -1,10 +1,26 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
+import BasicMenu from "../Header/menu";
 
 const NewHeader = () => {
   const [scrollY, setScrollY] = useState(0);
+  const router = useRouter();
+  // const [showMenu, setShowMenu] = useState(false);
+
+  const handleLoginNav = () => {
+    router.push("/login");
+  };
+
+  const handleMenuClick = () => {
+    setShowMenu(!showMenu);
+  };
+
+  // const handleMapNav = () => {
+  //   router.push('/ayatrio-map');
+  // }
 
   useEffect(() => {
     const handleScroll = () => {
@@ -19,74 +35,72 @@ const NewHeader = () => {
   const logoStyle =
     scrollY > 0 ? { width: 100, height: 16 } : { width: 150, height: 32 };
   return (
-    <div className="flex flex-col">
-      <div className="flex flex-row items-center">
-        <div className="sm:px-10 sm:pt-3 flex w-1/3 justify-start px-1 pt-2">
-          <button className="flex gap-3">
-            <span>
+    <>
+      <div className="flex flex-col">
+        <div className="flex flex-row items-center">
+          <div className="sm:px-10 sm:pt-3 flex w-1/3 justify-start px-1 pt-2">
+            <button onClick={handleMenuClick} className="flex gap-3">
+              <span>
+                <BasicMenu />
+              </span>
+            </button>
+          </div>
+          <div className="sm:gap-5 sm:px-10 sm:pt-3 flex flex-grow gap-2 justify-end px-1 pt-3">
+            <div className="cursor-pointer">
               <Image
-                src="/NewHeaderIcons/IndianFlag.svg"
+                src="/NewHeaderIcons/search.svg"
                 width={25}
                 height={25}
-                alt="India"
+                alt="search"
               />
-            </span>
-            <span>
-              <p className="text-sm justify-start">IN</p>
-            </span>
-          </button>
-        </div>
-        <div className="sm:gap-5 sm:px-10 sm:pt-3 flex flex-grow gap-2 justify-end px-1 pt-3">
-          <div>
-            <Image
-              src="/NewHeaderIcons/search-icon.svg"
-              width={25}
-              height={25}
-              alt="search"
-            />
-          </div>
-          <div>
-            <Image
-              src="/NewHeaderIcons/location-pin-icon.svg"
-              width={25}
-              height={25}
-              alt="location"
-            />
-          </div>
-          <div>
-            <Image
-              src="/NewHeaderIcons/profile-icon.svg"
-              width={25}
-              height={25}
-              alt="profile"
-            />
-          </div>
-          <div>
-            <Image
-              src="/NewHeaderIcons/shopping-bag-icon.svg"
-              width={25}
-              height={25}
-              alt="bag"
-            />
+            </div>
+            <div
+              // onClick={handleMapNav}
+              className="cursor-pointer"
+            >
+              <Image
+                src="/NewHeaderIcons/location-pin-icon.svg"
+                width={25}
+                height={25}
+                alt="location"
+              />
+            </div>
+            <div onClick={handleLoginNav} className="cursor-pointer">
+              <Image
+                src="/NewHeaderIcons/profile.svg"
+                width={25}
+                height={25}
+                alt="profile"
+              />
+            </div>
+            <div className="cursor-pointer">
+              <Image
+                src="/NewHeaderIcons/adtocart.svg"
+                width={25}
+                height={25}
+                alt="bag"
+              />
+            </div>
           </div>
         </div>
-      </div>
-      <div className="flex justify-center items-center">
-        <div
-          className={`flex justify-center ${logoStyle}`}
-          style={{ transition: "width 0.3s, height 0.4s", ...logoStyle }}
-        >
-          <Link href="/">
-            <Image
-              src="/ayatriologo.png"
-              width={logoStyle.width}
-              height={logoStyle.height}
-              alt="Ayatrio"
-            />
-          </Link>
+        <div className="flex justify-center items-center">
+          <div
+            className={`flex justify-center ${logoStyle}`}
+            style={{ transition: "width 0.3s, height 0.4s", ...logoStyle }}
+          >
+            <Link href="/">
+              <Image
+                src="/ayatriologo.png"
+                width={logoStyle.width}
+                height={logoStyle.height}
+                alt="Ayatrio"
+              />
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+      {/* {showMenu ?  : ""} */}
+    </>
   );
 };
 
