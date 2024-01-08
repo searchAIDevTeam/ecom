aae434d889260bf568cc59080505a83976f887c4
Karthik ks <karthikks260703@gmail.com>
2024-01-08 23:22:36 +0530

    map popup added

--- components/Map/MapMarker/index.js
+++ components/Map/MapMarker/index.js
@@ -1,21 +1,22 @@
 import React, { useState } from "react";
+import ReactDOM from "react-dom";
 import "./styles.css";
-// import Popover from "@mui/material/Popover";
+
+const PopupPortal = ({ children }) => {
+  return ReactDOM.createPortal(children, document.body);
+};
 
 const MapMarker = ({ place }) => {
-  const [anchorEl, setAnchorEl] = useState(null);
+  const [isPopupOpen, setPopupOpen] = useState(false);
 
-  const handleMarkerClick = (event) => {
+  const handleMarkerClick = () => {
     console.log("Marker clicked", place?.name);
-    setAnchorEl(event.currentTarget);
+    setPopupOpen(!isPopupOpen);
   };
 
   const handleClose = () => {
-    setAnchorEl(null);
+    setPopupOpen(false);
   };
-
-  const open = Boolean(anchorEl);
-  const id = open ? "simple-popover" : undefined;
 
   console.log("Rendering MapMarker for", place);
 
@@ -23,11 +24,7 @@ const MapMarker = ({ place }) => {
     <div className="marker-container gmap-marker">
       {place && place.name && place.thumbnail && (
         <>
-          <div
-            className="marker-info"
-            aria-describedby={id}
-            onClick={handleMarkerClick}
-          >
+          <div className="marker-info" onClick={handleMarkerClick}>
             <div className="info-wrapper wrapper">
               <div className="info-title">
                 <span className="title-text">{place.name}</span>
@@ -41,36 +38,39 @@ const MapMarker = ({ place }) => {
             </div>
           </div>
 
-          {/* <Popover
-            id={id}
-            open={open}
-            anchorEl={anchorEl}
-            onClose={handleClose}
-            anchorOrigin={{
-              vertical: "bottom",
-              horizontal: "left",
-            }}
-          >
-            {place.images && place.images.length > 0 && (
-              <div className="popoverMain marker-popover-image bg-white p-4">
-                <img
-                  className="rounded"
-                  src={place.images[0]}
-                  alt={place.name}
-                />
+          {isPopupOpen && (
+            <PopupPortal>
+              <div className="custom-popup fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-4 shadow-lg rounded-md w-[320px] h-[500px] mx-auto z-50">
+                <button
+                  className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+                  onClick={handleClose}
+                >
+                  Close
+                </button>
+                {place.images && place.images.length > 0 && (
+                  <>
+                    <img
+                      className="rounded w-full h-auto mt-4"
+                      src={place.images[0]}
+                      alt={place.name}
+                    />
 
-                <div className="text-gray-800 mt-4">
-                  <div className="flex flex-row items-center">
-                    <div className="text-xl font-semibold">{place.name}</div>
-                    <div className="ml-2 text-gray-500">
-                      {place.address.slice(0, 20)}
+                    <div className="text-gray-800 mt-4">
+                      <div className="flex flex-row items-center">
+                        <div className="text-xl font-semibold">
+                          {place.name}
+                        </div>
+                        <div className="ml-2 text-gray-500">
+                          {place.address.slice(0, 20)}
+                        </div>
+                      </div>
+                      <div className="mt-2 text-gray-600">{place.phone}</div>
                     </div>
-                  </div>
-                  <div className="mt-2 text-gray-600">{place.phone}</div>
-                </div>
+                  </>
+                )}
               </div>
-            )}
-          </Popover> */}
+            </PopupPortal>
+          )}
         </>
       )}
 
