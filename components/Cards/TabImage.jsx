1d170dde191038a3b02f31fc75cc611e447eefd5
Harshi Agrawal <114180714+harshi706@users.noreply.github.com>
2024-01-11 17:08:20 +0530

    label

--- components/Cards/TabImage.jsx
+++ components/Cards/TabImage.jsx
@@ -1,37 +1,14 @@
 import React from "react";
 import "./tabs.css";
-import { BsArrowRightCircleFill } from "react-icons/bs";
 import Image from "next/image";
-const TabImage = ({ src, alt, handleTab, circled }) => {
+import Label from "../Label/Label";
+const TabImage = ({ src, alt }) => {
   return (
     <div className="row-span-2 overflow-hidden relative">
       <Image className="h-full w-full object-cover" src={src} alt={alt} />
-      <div className="text-md text-black absolute top-16 left-10  bg-gray-500 rounded-full w-5 h-5">
-      <div className="bg-white h-3 w-3 absolute rounded-full ml-1 mt-1">
-        <div
-          className={`flex-row z-10 mt-4 w-36 h-32 flex items-center pb-2 bg-white cursor-pointer`}
-          onClick={handleTab}
-        >
-          <div className="flex flex-row relative">
-            {circled.map((data) => (
-              <div
-                className="flex flex-col basis-3/4 w-36 flex-grow relative ml-1 mr-2.5 pr-2"
-                key={data.productId}
-              >
-                <h2 className="font-bold pt-1 pr-2">{data.productTitle}</h2>
-                <p className="font-normal pb-2">{data.productCategory}</p>
-                <p className="font-bold bg-yellow-400 h-8 w-16 pl-2 main">
-                  ₹{data.price}
-                </p>
-              </div>
-            ))}
-            <div className="absolute right-0 top-0 border-l border-black flex justify-end items-center h-full pr-2">
-              <BsArrowRightCircleFill className="flex ml-2 mr-2" />
-            </div>
-          </div>
+      <div className="carcular-conui top-16 left-16  absolute rounded-full w-[30px] h-[30px]">
+        <Label/>
         </div>
-        </div>
-      </div>
     </div>
   );
 };
--- components/Product/meausrement.jsx
+++ components/Product/meausrement.jsx
@@ -1,20 +1,9 @@
 import React from "react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import Label from "../Label/Label";
+
+
 const meausrement = () => {
-  const circled = [
-    {
-      top: 50,
-      left: 68,
-      productTitle: "Bedroomhk...",
-      productCategory: "Mountain view",
-      price: 1900,
-    },
-  ];
-  const router = useRouter();
-  const handleTab = () => {
-    router.push("/room");
-  };
   return (
     <div className="flex sm:flex-row w-full h-auto flex-col sm:px-[50px] px-[20px] relative overflow-hidden ">
       <div className="relative max-h-[500px] ">
@@ -25,89 +14,25 @@ const meausrement = () => {
           height={300}
           style={{ objectFit: "cover" }}
         />
-        <div className="text-md text-black carcular-conui  absolute top-16 left-10 rounded-full w-[30px] h-[30px]">
-          <div className=" bg-white h-3 w-3 absolute rounded-full carcular-ui">
-            <div
-              className={`flex-row z-10 mt-6 box-container-product w-fit h-auto flex items-center  bg-white cursor-pointer`}
-              onClick={handleTab}
-            >
-              <div className="flex flex-row relative">
-                {circled.map((data) => (
-                  <div
-                    className="flex flex-col  basis-3/4 w-max flex-grow relative p-2.5"
-                    key={data.productId}
-                  >
-                    <h2 className="font-bold pt-1 pr-2">{data.productTitle}</h2>
-                    <p className="font-normal pb-2">{data.productCategory}</p>
-                    <p className="font-bold bg-yellow-400 h-8 w-16 pl-2 main">
-                      ₹{data.price}
-                    </p>
-                  </div>
-                ))}
-                <div className="absolute right-0 top-0 border-l border-black flex justify-end items-center h-full pr-1">
-                  <Image
-                    className="flex ml-2 mr-2"
-                    src="/backarrowRevarce.svg"
-                    height={20}
-                    width={20}
-                    alt="arrow"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
+      <div className="carcular-conui top-16 left-48  absolute rounded-full w-[30px] h-[30px]">
+        <Label/>
         </div>
-
-        <div className="text-md text-black carcular-conui absolute top-8 left-[26rem] rounded-full w-[30px] h-[30px]">
-          <div className=" bg-white h-3 w-3 absolute rounded-full carcular-ui">
-            <div
-              className={`flex-row z-10 mt-6 box-container-product w-fit h-auto flex items-center  bg-white cursor-pointer`}
-              onClick={handleTab}
-            >
-              <div className="flex flex-row relative">
-                {circled.map((data) => (
-                  <div
-                    className="flex flex-col basis-3/4 w-max flex-grow relative p-2.5"
-                    key={data.productId}
-                  >
-                    <h2 className="font-bold pt-1 pr-2">{data.productTitle}</h2>
-                    <p className="font-normal pb-2">{data.productCategory}</p>
-                    <p className="font-bold bg-yellow-400 h-8 w-16 pl-2 main">
-                      ₹{data.price}
-                    </p>
-                  </div>
-                ))}
-                <div className="absolute right-0 top-0 border-l border-black flex justify-end items-center h-full pr-1">
-                  <Image
-                    className="flex ml-2 mr-2"
-                    src="/backarrowRevarce.svg"
-                    height={20}
-                    width={20}
-                    alt="arrow"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
+        <div className="carcular-conui absolute top-8 left-[26rem] rounded-full w-[30px] h-[30px]">
+          <Label />
         </div>
       </div>
       <div className="sm:w-1/3  sm:h-auto sm:flex-grow bg-zinc-100  px-10 sm:py-10 py-5">
         <div className="flex flex-col ">
-
-          <div>Where we found happyness</div>
+          <div>Where we found happiness</div>
           <div>
-
             <p>"When you design your house, you tend to do everything that is right for you. Ayatrio has actually turned our dream into reality"</p>
           </div>
-
           <div className="flex flex-row feedcon">
-
             <Image src="/sunita chayan.JPG" width={45} height={45} alt="arrow" className="usercon rounded-full" />
             <p>Sunitha Roy</p>
           </div>
         </div>
       </div>
-
     </div>
   );
 };
