50bc5e83042b65eb21384b01da6dc9c954608455
amaan35 <amaanlm35@gmail.com>
2024-01-14 21:00:24 +0530

    Creating reusable component which works with swiper, added MultiCardData to map into slides containing MultiCardContent

---
+++ Model/MultiCard/MultiCardData.js
@@ -0,0 +1,37 @@
+export const multiCardData = [
+    {
+        id:1,
+        iconPath:"/MultiCardIcons/truck-delivery-icon.svg",
+        iconSize:52,
+        title:"Get flexible delivery and easy pickup.",
+        text:"Choose two-hour delivery from an Apple Store, free delivery, or easy pickup options."
+    },
+    {
+        id:2,
+        iconPath:"/MultiCardIcons/chat-bubbles.svg",
+        iconSize:46,
+        title:"Shop with a specialist over video",
+        text:"Choose your next Mac in a  guided, one-way video session. or easy pickup options."
+    },
+    {
+        id:3,
+        iconPath:"/MultiCardIcons/two-contacts.svg",
+        iconSize:48,
+        title:"Get to know your new Mac. need to added",
+        text:"Learn how to get the most out of your new Mac with a free one-on-one Personal Session."
+    },
+    {
+        id:4,
+        iconPath:"/MultiCardIcons/book.svg",
+        iconSize:44,
+        title:"Get flexible delivery and easy pickup.",
+        text:"Learn how to get the most out of your new Mac with a free one-on-one Personal Session."
+    },
+    {
+        id:5,
+        iconPath:"/MultiCardIcons/book.svg",
+        iconSize:44,
+        title:"Get flexible delivery and easy pickup.",
+        text:"Learn how to get the most out of your new Mac with a free one-on-one Personal Session."
+    },
+]
