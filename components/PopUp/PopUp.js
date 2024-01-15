151c6e95bb0e5355be893313b3b924c9c13f4deb
amaan35 <amaanlm35@gmail.com>
2024-01-15 16:39:59 +0530

    Adding background shadow on popup display

--- components/PopUp/PopUp.js
+++ components/PopUp/PopUp.js
@@ -255,7 +255,11 @@ function App() {
                             }
                             style={{ background: `url('${subcategory.img}')` }}
                           >
-                            <Image src='/circletick.svg' height={20} width={20} alt='tick'
+                            <Image
+                              src="/circletick.svg"
+                              height={20}
+                              width={20}
+                              alt="tick"
                               className={`absolute tickicon ${
                                 selectedItems1.includes(subcategory)
                                   ? "block"
@@ -296,7 +300,11 @@ function App() {
                         backgroundSize: "center",
                       }}
                     >
-                  <Image src='/circletick.svg' height={20} width={20} alt='close'
+                      <Image
+                        src="/circletick.svg"
+                        height={20}
+                        width={20}
+                        alt="close"
                         className={`absolute tickicon ${
                           selectedItems2.includes(city) ? "block" : "hidden"
                         }`}
@@ -343,7 +351,11 @@ function App() {
                         }}
                         key={index}
                       >
-                  <Image src='/circletick.svg' height={20} width={20} alt='close'
+                        <Image
+                          src="/circletick.svg"
+                          height={20}
+                          width={20}
+                          alt="close"
                           className={`absolute tickicon ${
                             selectedItems3.includes(hobbie) ? "block" : "hidden"
                           }`}
