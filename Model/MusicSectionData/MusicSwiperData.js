0a066c1f8598dc4288607b71d769e5bce4dae03f
amaan35 <amaanlm35@gmail.com>
2024-01-17 23:26:15 +0530

    Created swiper for music section with overflow effect

---
+++ Model/MusicSectionData/MusicSwiperData.js
@@ -0,0 +1,26 @@
+export const musicSwiperData = [
+    {
+      id: 1,
+      imgSrc: "/images/cover.webp",
+      title: "Bossman Dlow",
+      subTitle: "Slippery",
+    },
+    {
+      id: 2,
+      imgSrc: "/images/cover.webp",
+      title: "Bossman Dlow",
+      subTitle: "Slippery",
+    },
+    {
+      id: 3,
+      imgSrc: "/images/cover.webp",
+      title: "Bossman Dlow",
+      subTitle: "Slippery",
+    },
+    {
+      id: 4,
+      imgSrc: "/images/cover.webp",
+      title: "Bossman Dlow",
+      subTitle: "Slippery",
+    },
+  ];
