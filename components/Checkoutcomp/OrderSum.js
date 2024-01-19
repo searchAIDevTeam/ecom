b0ba58be403547ce47c28ff5f31b22bf2c1d274f
SOURADEEP HAZRA <V22089@students.iitmandi.ac.in>
2024-01-20 01:19:10 +0530

    added 404 page

--- components/Checkoutcomp/OrderSum.js
+++ components/Checkoutcomp/OrderSum.js
@@ -33,61 +33,62 @@ const OrderSum = () => {
 
   return (
     <div className="sm:w-auto w-[80vw]">
-      {properties === "rooms" && roomStatus === "succeeded" && (
-        <div>
-          <h3 className=" font-lg text-2xl">Order Summary</h3>
-          <br />
-          <div className="grid grid-cols-2">
-            <div className="col-span-1 text-gray-500 flex flex-col gap-2">
-              Subtotal <br /> <br />
-              Delivery/Shipping
-              <br />
-              <hr />
-              <h3 className="mt-2 mb-2 text-black">Total</h3>
-              <hr />
-            </div>
-            <div className="col-span-1 ">
-              ₹{roomData.totalPrice * quantity}
-              <br />
-              <br />
-              ₹ 1250.00
-              <br />
-              <br />₹ {roomData.totalPrice * quantity + 1250.0}
-              <br />
-            </div>
-          </div>
-          <h6 className="text-xs mt-2 mb-6 sm:w-[90%] w-[70vw]">
-            (The total reflects the price of your order, including all duties
-            and taxes)
-          </h6>
-          {/* from here show start */}
-          <h3 className="font-bold">Arrives Thu, 13 Dec-Wed, 3 Jan</h3>
-          <div className="grid grid-cols-2 gap-10">
-            <div className="col-span-1">
-              {/* <img src={shoe} alt='shoes'/> */}
-              <Image
-                src={roomData.images[0]}
-                alt="shoes"
-                width={180}
-                height={180}
-              />
-            </div>
-            <div className="col-span-1">
-              <h5 className="text-sm">
-                {roomData.productTitle}
+      {(properties === "rooms" || properties === "sample") &&
+        roomStatus === "succeeded" && (
+          <div>
+            <h3 className=" font-lg text-2xl">Order Summary</h3>
+            <br />
+            <div className="grid grid-cols-2">
+              <div className="col-span-1 text-gray-500 flex flex-col gap-2">
+                Subtotal <br /> <br />
+                Delivery/Shipping
                 <br />
-                {roomData.category}
-              </h5>
-              <h6 className="text-sm text-gray-500">Qty : {quantity}</h6>
-              <h6 className="text-sm text-gray-500">Size UK 7.5</h6>
-              <h6 className="text-sm text-gray-500">
-                {" "}
-                ₹{roomData.totalPrice + 1250.0}
-              </h6>
+                <hr />
+                <h3 className="mt-2 mb-2 text-black">Total</h3>
+                <hr />
+              </div>
+              <div className="col-span-1 ">
+                ₹{roomData.totalPrice * quantity}
+                <br />
+                <br />
+                ₹ 1250.00
+                <br />
+                <br />₹ {roomData.totalPrice * quantity + 1250.0}
+                <br />
+              </div>
+            </div>
+            <h6 className="text-xs mt-2 mb-6 sm:w-[90%] w-[70vw]">
+              (The total reflects the price of your order, including all duties
+              and taxes)
+            </h6>
+            {/* from here show start */}
+            <h3 className="font-bold">Arrives Thu, 13 Dec-Wed, 3 Jan</h3>
+            <div className="grid grid-cols-2 gap-10">
+              <div className="col-span-1">
+                {/* <img src={shoe} alt='shoes'/> */}
+                <Image
+                  src={roomData.images[0]}
+                  alt="shoes"
+                  width={180}
+                  height={180}
+                />
+              </div>
+              <div className="col-span-1">
+                <h5 className="text-sm">
+                  {roomData.productTitle}
+                  <br />
+                  {roomData.category}
+                </h5>
+                <h6 className="text-sm text-gray-500">Qty : {quantity}</h6>
+                <h6 className="text-sm text-gray-500">Size UK 7.5</h6>
+                <h6 className="text-sm text-gray-500">
+                  {" "}
+                  ₹{roomData.totalPrice + 1250.0}
+                </h6>
+              </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
 
       {properties === "cart" && (
         <div>
@@ -118,37 +119,39 @@ const OrderSum = () => {
           </h6>
           {/* from here show start */}
           <div>
-            {dbItemset && dbItemset.items && dbItemset.items.map((item) => (
-              <div key={item._id}>
-                <h3 className="font-bold">Arrives Thu, 13 Dec-Wed, 3 Jan</h3>
-                <div className="grid grid-cols-2 gap-10">
-                  <div className="col-span-1">
-                    {/* <img src={shoe} alt='shoes'/> */}
-                    <Image
-                      src={item.productId.images[0]}
-                      alt="shoes"
-                      width={180}
-                      height={180}
-                    />
-                  </div>
-                  <div className="col-span-1">
-                    <h5 className="text-sm">
-                      {item.productId.productTitle}
-                      <br />
-                      {item.productId.category}
-                    </h5>
-                    <h6 className="text-sm text-gray-500">
-                      Qty : {item.quantity}
-                    </h6>
-                    <h6 className="text-sm text-gray-500">Size UK 7.5</h6>
-                    <h6 className="text-sm text-gray-500">
-                      {" "}
-                      ₹ {item.quantity * item.price}
-                    </h6>
+            {dbItemset &&
+              dbItemset.items &&
+              dbItemset.items.map((item) => (
+                <div key={item._id}>
+                  <h3 className="font-bold">Arrives Thu, 13 Dec-Wed, 3 Jan</h3>
+                  <div className="grid grid-cols-2 gap-10">
+                    <div className="col-span-1">
+                      {/* <img src={shoe} alt='shoes'/> */}
+                      <Image
+                        src={item.productId.images[0]}
+                        alt="shoes"
+                        width={180}
+                        height={180}
+                      />
+                    </div>
+                    <div className="col-span-1">
+                      <h5 className="text-sm">
+                        {item.productId.productTitle}
+                        <br />
+                        {item.productId.category}
+                      </h5>
+                      <h6 className="text-sm text-gray-500">
+                        Qty : {item.quantity}
+                      </h6>
+                      <h6 className="text-sm text-gray-500">Size UK 7.5</h6>
+                      <h6 className="text-sm text-gray-500">
+                        {" "}
+                        ₹ {item.quantity * item.price}
+                      </h6>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
           </div>
         </div>
       )}
