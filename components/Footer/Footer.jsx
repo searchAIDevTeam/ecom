c373262f4bf211ba627d702ccf153d06e1dabd6c
amaan35 <amaanlm35@gmail.com>
2024-01-20 18:02:56 +0530

    Creating new reusable footer component, deleted previous 4 components that were being used to create footer, also reducing unused CSS and converting few to tailwind, completed adding corresponding routes in footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,42 +1,28 @@
 import React from "react";
 import "./styles.css";
 import JoinAyatrioFamily from "./Footer_child/JoinAyatrioFamily";
-import AyatrioFamily from "./Footer_child/AyatrioFamily";
-import AyatrioServices from "./Footer_child/AyatrioServices";
-import HelpSupports from "./Footer_child/HelpSupports";
-import AboutAyatrio from "./Footer_child/AboutAyatrio";
+import { footerData } from "../../Model/FooterColumnData/FooterColumnData";
+import FooterContent from "../molecules/FooterContent";
 
 const Footer = () => {
   return (
-    <>
-      <div className="sm:px-[50px] px-[20px] mt-20 pt-10 footer-container aspect-auto w-full h-[500px] sm:mb-0 mb-10 ">
-        <div className="filter_container flex gap-32 w-full h-auto ">
-          <div className="grid sm:grid-cols-6 grid-cols-1">
-            <div className="sm:col-span-2 row-span-1 col-span-1 mr-12 mb-5">
-              <JoinAyatrioFamily />
-            </div>
-            <hr className=" border w-[75vw] sm:hidden block" />
-            <div className="col-span-1">
-              <AyatrioFamily />
-            </div>
-            <hr className=" border w-[75vw] sm:hidden block" />
-            <div className="col-span-1">
-              <AyatrioServices />
-            </div>
-            <hr className=" border w-[75vw] sm:hidden block" />
-            <div className="col-span-1">
-              <HelpSupports />
-            </div>
-            <hr className=" border w-[75vw] sm:hidden block" />
-            <div className="col-span-1">
-              <AboutAyatrio />
-            </div>
-            <hr className=" border w-[75vw] sm:hidden block" />
-          </div>
+    <div className="bg-gray-100 sm:px-[50px] px-[50px] mt-20 py-[70px]">
+      <div className="grid sm:grid-cols-6 grid-cols-1 pb-5">
+        <div className="sm:col-span-2 row-span-1 col-span-1 mr-12 mb-5">
+          <JoinAyatrioFamily />
         </div>
+        <hr className="border w-[75vw] sm:hidden block" />
+        {footerData.map((column) => {
+          return (
+            <FooterContent
+              headingId={column.id}
+              categoryHeading={column.categoryHeading}
+              categoryData={column.categoryData}
+            />
+          );
+        })}
       </div>
-
-      <div className="flex sm:px-[50px] px-[20px]  footer_bg sm:flex-row flex-col justify-between p-4">
+      <div className="flex sm:px-[50px] px-[20px] sm:border-t-2 sm:border-solid sm:border-gray-200 sm:flex-row flex-col justify-between p-4">
         <div className="flex  items-center space-x-4 text-lg font-semibold tracking-tight">
           <ul className="flex flex-wrap items-center justify-center text-gray-900 dark:text-white">
             <li>
@@ -103,7 +89,7 @@ const Footer = () => {
           </ul>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
