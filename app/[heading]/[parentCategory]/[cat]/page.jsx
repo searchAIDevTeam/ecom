6a78ad270d5183406cbcedd9fee008b97eabad7c
Karthik ks <karthikks260703@gmail.com>
2024-01-20 22:37:35 +0530

    added dynamic metadata

--- app/[heading]/[parentCategory]/[cat]/page.jsx
+++ app/[heading]/[parentCategory]/[cat]/page.jsx
@@ -1,69 +1,19 @@
-'use client'
-import React, { useEffect, useState } from "react";
-// import Header from "../components/Header";
-import Products from "../../../../components/Product/Products";
-// import "../../components/Product/styles.css";
-// import "../../../../components/Product/styles.css"; 
-import "@/components/Product/styles.css";
-// import Footer from "../components/Footer/Footer";
-import Measure from "@/components/Product/meausrement";
-import Tabproduct from "@/components/Product/TabsProducts";
-import axios from "axios";
+import React from "react";
+import ProductPage from "../Meta";
 
-// const hideheader=
-const ProductPage = ({params}) => {
-  const [isFilterVisible, setIsFilterVisible] = useState(true);
-  const [filteredProducts, setFilteredProducts] = useState([]);
-  
-  let parentCategoryVar = params.parentCategory;
-  console.log("heading : ",params.heading);
-
-useEffect(() => {
-  const fetchFilteredProducts = async () => {
-    try {
-      // Constructing the URL dynamically based on params
-      const apiUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/products?category=${encodeURIComponent(params.heading)}&${parentCategoryVar}=${encodeURIComponent(params.cat)}`;
-      
-      // Fetch products using the dynamically constructed URL
-      const response = await axios.get(apiUrl);
-
-      // Handle the fetched products as needed
-      // console.log("Filtered products:", response.data);
-      setFilteredProducts(response.data); // Save the filtered products in state
-    } catch (error) {
-      console.error("Error fetching filtered products:", error);
-    }
-  };  
-  fetchFilteredProducts();
+export function generateMetadata({ params, searchParams }, parent) {
+  // console.log("f", params, searchParams);
+  return {
+    title: decodeURIComponent(params.cat),
+  };
 }
-, [params.parentCategory, params.cat]);
-  useEffect(() => {
-    let prevScrollPos = window.scrollY;
 
-    const handleScroll = () => {
-      const currentScrollPos = window.scrollY;
-      setIsFilterVisible(
-        currentScrollPos <= prevScrollPos || currentScrollPos < 100
-      );
-      prevScrollPos = currentScrollPos;
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
+const page = ({ params }) => {
   return (
-    <div>
-      {/* {isFilterVisible && <Header />} */}
-      <Products filteredProducts={filteredProducts} />
-      <Tabproduct filteredProducts={filteredProducts} heading={params.heading}/>
-      <Measure filteredProducts={filteredProducts}/>
-      {/* <Footer /> */}
-    </div>
+    <>
+      <ProductPage params={params} />
+    </>
   );
 };
 
-export default ProductPage;
+export default page;
