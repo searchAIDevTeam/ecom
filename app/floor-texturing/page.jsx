419470bd8a9aa7ff4d8d72ff9e1f71e7d9a6653a
jahid28 <jahidrhps123@gmail.com>
2024-02-08 23:19:58 +0530

    comparator-updated

---
+++ app/floor-texturing/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Floortexturing from "../../components/FloorTexturing"
+const page = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default page
