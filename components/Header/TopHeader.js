e776f44318488cdd5d9157b4b21135c14ebc3754
SOURADEEP HAZRA <V22089@students.iitmandi.ac.in>
2024-02-09 17:31:39 +0530

    updated mainslider,added grey background, height updated,topheader given to width of screen,header given to width of screen

--- components/Header/TopHeader.js
+++ components/Header/TopHeader.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -6,49 +6,27 @@ import { usePathname } from "next/navigation";
 const TopHeader = () => {
   const pathname = usePathname();
   const homeRoute = "/home";
-  const [isFilterVisible, setIsFilterVisible] = useState(true);
-
-  useEffect(() => {
-    let prevScrollPos = window.scrollY;
-
-    const handleScroll = () => {
-      const currentScrollPos = window.scrollY;
-      setIsFilterVisible(
-        currentScrollPos <= prevScrollPos || currentScrollPos < 100
-      );
-      prevScrollPos = currentScrollPos;
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
     <div>
       <div
         className={`bg-zinc-200 ${
           homeRoute === pathname ? "fixed" : ""
-        } z-[99999] w-full flex flex-row justify-between 
-      ${isFilterVisible ? "block" : "hidden"}
-      `}
+        } z-[99999] w-screen flex flex-row justify-between`}
       >
         <div className="pl-8 pt-2 pb-2">
           <Image
             src="/logo/favicon-32x32.png"
-            height={22}
-            width={22}
+            height={20}
+            width={20}
             alt="logo"
           />
         </div>
         <div>
-          <div className="flex flex-row gap-2 text-gray-500 text-sm pt-2 pr-4">
-            <Link href="/virtualexperience/category">Virtual Experience |</Link>
-            <Link href="/designservice">Free Design Request &nbsp; &nbsp;|</Link>
-            <Link href="/freesample">Free Sample Request &nbsp; &nbsp;|</Link>
-            <Link href="/login">Join Ayatrio Family</Link>
+          <div className="flex flex-row gap-2 text-gray-500 text-sm pt-2 pr-5">
+            <Link href="/">Virtual Experience |</Link>
+            <Link href="/">Free Design |</Link>
+            <Link href="/">Free Sample |</Link>
+            <Link href="/">Join Ayatrio Family</Link>
           </div>
         </div>
       </div>
