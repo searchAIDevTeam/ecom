30f975f9ff70efff4cff16aaf848347daf32f912
Karthik ks <karthikks260703@gmail.com>
2024-02-09 18:26:55 +0530

    Revert "fixed errors and dropdown issue"
    This reverts commit 8f2010e9e3fa0f034f4aba357643d3b249963f0f.

--- components/compounds/TabsProductContent.js
+++ components/compounds/TabsProductContent.js
@@ -1,14 +1,6 @@
-"use client";
-
 import React from "react";
 import "@/components/Product/styles.css";
 import Image from "next/image";
-import {
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-  Button,
-} from "@nextui-org/react";
 
 const TabsProductContent = (props) => {
   const {
@@ -30,10 +22,8 @@ const TabsProductContent = (props) => {
 
   return (
     <div>
-      <Popover
-        placement="bottom-start"
-        isOpen={isFilterOpen}
-        onOpenChange={() => {
+      <button
+        onClick={() => {
           if (typeof window !== "undefined" && window.innerWidth <= 450) {
             handleAll();
             handleTabClick();
@@ -43,66 +33,57 @@ const TabsProductContent = (props) => {
             handleTabClick();
           }
         }}
+        className={`
+                  ${
+                    isFilterOpen
+                      ? `active-tabs  border border-black ${commonClasses}`
+                      : `tabS  border border-white ${commonClasses}`
+                  }
+                  ${() =>
+                    typeof window !== "undefined" && window.innerWidth <= 450
+                      ? " justify-center"
+                      : " justify-between"}
+                  `}
       >
-        <PopoverTrigger>
-          <button
-            className={`
-                      ${
-                        isFilterOpen
-                          ? `active-tabs  border border-black ${commonClasses}`
-                          : `tabS  border border-white ${commonClasses}`
-                      }
-                      ${() =>
-                        typeof window !== "undefined" &&
-                        window.innerWidth <= 450
-                          ? " justify-center"
-                          : " justify-between"}
-                      `}
-          >
-            {filterName} &nbsp;
-            <Image
-              src="/svg/dropdown/backarrow.svg"
-              width={40}
-              height={40}
-              className={`w-6 h-6  mt-1 sm:block hidden
-                    ${isFilterOpen ? " rotate-90" : "-rotate-90"}
-                    
-                    `}
-              alt=""
-            />
-          </button>
-        </PopoverTrigger>
-
-        <PopoverContent>
-          {isFilterOpen ? (
-            <div className="flex flex-col px-5 py-5 overflow-y-auto bg-white border gap-7 rounded-2xl w-72 h-80">
-              {filterArr.map(renderFilter)}
-              {filterName === "Type" ? (
-                <button
-                  className={`text-left underline
-                      ${openContent ? "hidden" : "block"}
-                      `}
-                  onClick={handleContent}
-                >
-                  +7 more
-                </button>
-              ) : null}
-              {openContent ? typeContent.map(renderTypeContent) : null}
-
-              {filterName === "Type" && openContent ? (
-                <button
-                  onClick={handleContent}
-                  className={`text-left underline ${
-                    openContent ? "block" : "hidden"
-                  }`}
-                >
-                  Less
-                </button>
-              ) : null}
-            </div>
+        {filterName} &nbsp;
+        <Image
+          src="/svg/dropdown/backarrow.svg"
+          width={40}
+          height={40}
+          className={`w-6 h-6  mt-1 sm:block hidden
+                ${isFilterOpen ? " rotate-90" : "-rotate-90"}
+                
+                `}
+          alt=""
+        />
+      </button>
+      {isFilterOpen ? (
+        <div className=" bg-white flex flex-col  gap-7 py-5 rounded-2xl w-72 border h-80 overflow-y-auto px-5">
+          {filterArr.map(renderFilter)}
+          {filterName === "Type" ? (
+            <button
+              className={`text-left underline
+                  ${openContent ? "hidden" : "block"}
+                  `}
+              onClick={handleContent}
+            >
+              +7 more
+            </button>
           ) : null}
-        </PopoverContent>
-      </Popover>
+          {openContent ? typeContent.map(renderTypeContent) : null}
+
+          {filterName === "Type" && openContent ? (
+            <button
+              onClick={handleContent}
+              className={`text-left underline ${
+                openContent ? "block" : "hidden"
+              }`}
+            >
+              Less
+            </button>
+          ) : null}
+        </div>
+      ) : null}
     </div>
   );
 };
