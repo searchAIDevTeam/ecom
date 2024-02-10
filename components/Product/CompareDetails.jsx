3e06dc5971440ade230c65e4b33c7233e5b0b309
jahid28 <jahidrhps123@gmail.com>
2024-02-10 13:27:46 +0530

    updated_image_comparator

--- components/Product/CompareDetails.jsx
+++ components/Product/CompareDetails.jsx
@@ -28,10 +28,10 @@ const CompareDetails = ({ filteredProducts }) => {
               className="  flex justify-center items-center flex-col gap-3"
               key={item._id}
             >
+              
               <p className="text-2xl font-semibold">{item.productTitle}</p>
 
               <p>
-                {" "}
                 ₹<span className="text-3xl">{item.perUnitPrice}</span>
               </p>
               <button
@@ -44,28 +44,27 @@ const CompareDetails = ({ filteredProducts }) => {
               <hr className=" bg-slate-900 w-full" />
               <div className="flex flex-col gap-5 items-center justify-center">
                 <div className="flex flex-col items-center justify-center">
-                  <p>Length</p>
-                  <p className="text-sm">{item.dimensions.length.value}</p>
+                  <p className="text-sm">Length: {item.dimensions.length.value}</p>
                 </div>
                 <div className="flex flex-col items-center justify-center">
-                  <p>Width</p>
-                  <p className="text-sm">{item.dimensions.width.value}</p>
+                  {/* <p>Width</p> */}
+                  <p className="text-sm">Width: {item.dimensions.width.value}</p>
                 </div>
                 <div className="flex flex-col items-center justify-center">
-                  <p>Category</p>
-                  <p className="text-sm">{item.category} </p>
+                  {/* <p>Category</p> */}
+                  <p className="text-sm">Category: {item.category} </p>
                 </div>
                 <div className="flex flex-col items-center justify-center">
-                  <p>Style</p>
-                  <p className="text-sm">{item.style} </p>
+                  {/* <p>Style</p> */}
+                  <p className="text-sm">Style: {item.style} </p>
                 </div>
                 <div className="flex flex-col items-center justify-center">
-                  <p>Features</p>
-                  <p className="text-sm">{item.features}</p>
+                  {/* <p>Features</p> */}
+                  <p className="text-sm">Features: {item.features}</p>
                 </div>
                 <div className="flex flex-col items-center justify-center">
-                  <p>Product description</p>
-                  <p className="text-sm">{item.productDescription} </p>
+                  {/* <p>Product description</p> */}
+                  <p className="text-sm">Product description: {item.productDescription} </p>
                 </div>
                 <div className="flex flex-col items-center justify-center">
                   <p>Rating</p>
@@ -81,7 +80,7 @@ const CompareDetails = ({ filteredProducts }) => {
                     ))}
                   </p>
                 </div>
-              </div>
+              </div> 
               <hr className=" bg-slate-900 w-full" />
             </div>
           ))}
--- components/Product/compareImage2.jsx
+++ components/Product/compareImage2.jsx
@@ -1,15 +1,20 @@
 "use client";
-import React, { useState } from "react";
+import React, { useRef, useEffect, useState } from "react";
+import * as THREE from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { useSelector } from "react-redux";
 import {
   selectproductdata,
   selectproductstatus,
 } from "../Features/Slices/compareSlice";
 import "./styled.css";
+import Image from "next/image";
 
 const CompareImage2 = () => {
   const data = useSelector(selectproductdata);
   const status = useSelector(selectproductstatus);
+
   // const beforeImage = datas[0].images[0];
   // const afterImage = datas[1].images[0];
   // const neoImage = datas[2].images[0];
@@ -28,24 +33,112 @@ const CompareImage2 = () => {
     <div className="App">
       {status === "succeeded" ? (
         <div>
-          <div className="text-center py-[20px] " >
+          <div className="text-center py-[20px] ">
             <h1 className="text-5xl py-[15px] ">Compare Products</h1>
-            <p>Get help choosing. <span className="text-blue-600"> Chat with a Specialist</span></p></div>
-          <div className="image-comparison">
-            <div className="images-container">
-              <img
-                className="before-image"
+            <p>
+              Get help choosing.{" "}
+              <span className="text-blue-600"> Chat with a Specialist</span>
+            </p>
+          </div>
+          <div className="image-comparison relative w-[90vw]">
+            <div className="images-container relative w-[100%] h-[100%]">
+              {/* <img
+                className="before-image absolute w-[10vw]"
                 src={data[0].images[0]}
                 alt=""
-                style={{ width: `${sliderValue}%` }}
-              />
-              {/* , left: `${sliderValue}` */}
-              <img
-                className="after-image" // Add a class for the "after.jpg" image
-                src={data[1].images[0]}
-                alt=""
-              // style={{ width: `${sliderValue}%` }}
-              />
+                style={{
+                  width: "100%",
+                  clipPath: `polygon(0% 0%, ${sliderValue}% 0%, ${sliderValue}% 100%, 0% 100%)`,
+                }}
+              /> */}
+              <div
+                className="before-image absolute  h-[100%]"
+                style={{
+                  width: "100%",
+                  clipPath: `polygon(0% 0%, ${sliderValue}% 0%, ${sliderValue}% 100%, 0% 100%)`,
+                }}
+              >
+                <div className="relative w-[100%] h-[100%] overflow-hidden">
+                  {/* <Image
+                    src="/room/tex_room.png"
+                    width={800}
+                    height={120}
+                    style={{width:"100"}}
+                    className="z-10"
+                    alt="not-found"
+                    // loading="lazy"
+                  /> */}
+                  <img
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      zIndex: "10",
+                    }}
+                    src="/room/tex_room.png"
+                    alt=""
+                  />
+
+                  <img
+                    style={{
+                      width: "60vw",
+                      height: "70vh",
+                      zIndex: "-1",
+                      transformStyle: "preserve-3d",
+                      perspective: "1000",
+                      transform:
+                        "scale(2.2) perspective(1200px) rotateX(75deg) rotateZ(-48deg) rotateY(0deg)",
+                    }}
+                    className="absolute left-0 top-[40px]"
+                    src={data[0].images[0]}
+                    alt=""
+                  />
+                </div>
+              </div>
+
+              <div
+                className="before-image absolute  h-[100%]"
+                style={{
+                  width: "100%",
+                  clipPath: `polygon(${sliderValue}% 0, 100% 0, 100% 100%, ${sliderValue}% 100%)`,
+                }}
+              >
+                <div className="relative w-[100%] h-[100%] overflow-hidden">
+                  {/* <Image
+                    src="/room/tex_room.png"
+                    width={800}
+                    height={120}
+                    // style={{width:"1000px"}}
+                    className="z-10"
+                    alt="not-found"
+                    // loading="lazy"
+                  /> */}
+
+                  <img
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      zIndex: "10",
+                    }}
+                    src="/room/tex_room.png"
+                    alt=""
+                  />
+
+                  <img
+                    style={{
+                      width: "60vw",
+                      height: "70vh",
+                      zIndex: "-1",
+                      transformStyle: "preserve-3d",
+                      perspective: "1000",
+                      transform:
+                        "scale(2.2) perspective(1200px) rotateX(75deg) rotateZ(-48deg) rotateY(0deg)",
+                    }}
+                    className="absolute left-0 top-[100px]"
+                    src={data[1].images[0]}
+                    alt=""
+                  />
+                </div>
+              </div>
               {/* <img
             className="neo-image"
             src={ datas[2].images[0]}
