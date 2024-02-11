1e0fa25a0e647560d8da3cccb44042ed750e24e1
amaan35 <amaanlm35@gmail.com>
2024-02-11 12:57:04 +0530

    Added design style dropdown, modified color design structure, mobile implementation remains

--- components/Product/tabsRender.jsx
+++ components/Product/tabsRender.jsx
@@ -1,5 +1,11 @@
+import Image from "next/image";
+
 export const renderSortItem = (text, idx, handleSorting) => (
-  <div className="flex justify-between" onClick={() => handleSorting(text)} key={idx}>
+  <div
+    className="flex justify-between"
+    onClick={() => handleSorting(text)}
+    key={idx}
+  >
     <label htmlFor="age1" className="">
       {text.name}
     </label>
@@ -7,71 +13,84 @@ export const renderSortItem = (text, idx, handleSorting) => (
   </div>
 );
 
-  export const rendersizewidth = (text, idx) => (
-    <div className="flex justify-between" key={idx}>
-      <label for="age1" className="">
-        {text.name}
-      </label>
-      <div className="flex gap-6">
-        <label>{text.value}</label>
-        <input type="checkbox" id="age6" name="age" value="36" />
-      </div>
+export const rendersizewidth = (text, idx) => (
+  <div className="flex justify-between" key={idx}>
+    <label for="age1" className="">
+      {text.name}
+    </label>
+    <div className="flex gap-6">
+      <label>{text.value}</label>
+      <input type="checkbox" id="age6" name="age" value="36" />
     </div>
-  );
-  export const rendercategory = (text, idx) => (
-    <div className="flex justify-between" key={idx}>
-      <label for="age1" className="">
-        {text.name}
-      </label>
-      <div className="flex gap-6">
-        <label>{text.value}</label>
-        <input type="checkbox" id="age6" name="age" value="36" />
-      </div>
-    </div>
-  );
+  </div>
+);
 
-  export const renderCollection = (text, idx) => (
-    <div className="flex justify-between" key={idx}>
-      <label for="age1" className="">
-        {text.name}
-      </label>
-      <div>
-        <input type="checkbox" id="age6" name="age" value="36" />
-      </div>
+export const renderColor = (text, idx) => (
+  <div className="flex justify-between items-center" key={idx}>
+    <label for="age1" className="">
+      {text.name}
+    </label>
+    <div className="flex gap-2">
+      <Image src={text.image} width={25} height={15} className="rounded-full w-5 h-5" />
+      <input type="checkbox" id="age6" name="age" value="36" />
     </div>
-  );
+  </div>
+);
 
-  export const rendersizeheight = (text, idx) => (
-    <div className="flex justify-between" key={idx}>
-      <label for="age1" className="">
-        {text.name}
-      </label>
-      <div className="flex gap-6">
-        <label>{text.value}</label>
-        <input type="checkbox" id="age6" name="age" value="36" />
-      </div>
+export const rendercategory = (text, idx) => (
+  <div className="flex justify-between" key={idx}>
+    <label for="age1" className="">
+      {text.name}
+    </label>
+    <div className="flex gap-6">
+      <label>{text.value}</label>
+      <input type="checkbox" id="age6" name="age" value="36" />
     </div>
-  );
-  export const renderTypeContent = (text, idx) => (
-    <div className="flex justify-between" key={idx}>
-      <label for="age1" className="">
-        {text.name}
-      </label>
-      <div className="flex gap-6">
-        <label>{text.value}</label>
-        <input type="checkbox" id="age6" name="age" value="36" />
-      </div>
-    </div>
-  );
+  </div>
+);
 
-  export const renderType = (text, idx) => (
-    <div className={`flex justify-between`} key={idx}>
-      <label for="age1" className="">
-        {text.name}
-      </label>
-      <div className="flex gap-6">
-        <label>{text.value}</label>
-        <input type="checkbox" id="age6" name="age" value="36" />
-      </div>
+export const renderCollection = (text, idx) => (
+  <div className="flex justify-between" key={idx}>
+    <label for="age1" className="">
+      {text.name}
+    </label>
+    <div>
+      <input type="checkbox" id="age6" name="age" value="36" />
     </div>
-  );
+  </div>
+);
+
+export const rendersizeheight = (text, idx) => (
+  <div className="flex justify-between" key={idx}>
+    <label for="age1" className="">
+      {text.name}
+    </label>
+    <div className="flex gap-6">
+      <label>{text.value}</label>
+      <input type="checkbox" id="age6" name="age" value="36" />
+    </div>
+  </div>
+);
+export const renderTypeContent = (text, idx) => (
+  <div className="flex justify-between" key={idx}>
+    <label for="age1" className="">
+      {text.name}
+    </label>
+    <div className="flex gap-6">
+      <label>{text.value}</label>
+      <input type="checkbox" id="age6" name="age" value="36" />
+    </div>
+  </div>
+);
+
+export const renderType = (text, idx) => (
+  <div className={`flex justify-between`} key={idx}>
+    <label for="age1" className="">
+      {text.name}
+    </label>
+    <div className="flex gap-6">
+      <label>{text.value}</label>
+      <input type="checkbox" id="age6" name="age" value="36" />
+    </div>
+  </div>
+);
