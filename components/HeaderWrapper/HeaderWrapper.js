a56936704a97194e2e5cd7590b5e79ae296c0338
Karthik ks <karthikks260703@gmail.com>
2024-02-12 16:40:29 +0530

    fixed hydration error

--- components/HeaderWrapper/HeaderWrapper.js
+++ components/HeaderWrapper/HeaderWrapper.js
@@ -1,7 +1,10 @@
 "use client";
+import dynamic from "next/dynamic";
+
 import { usePathname } from "next/navigation";
 import React from "react";
-import Header from "../Header";
+// import Header from "../Header";
+const Header = dynamic(() => import("../Header"), { ssr: false });
 
 const HeaderWrapper = () => {
   const pathname = usePathname();
